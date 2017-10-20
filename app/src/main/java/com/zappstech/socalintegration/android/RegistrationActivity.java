52a20d412b28c5684e71c734a799ffa55c4cfe96
rameshvelpukonda <velpukondaramesh@gmail.com>
2017-10-20 14:12:23 +0530

    added registration screen

---
+++ app/src/main/java/com/zappstech/socalintegration/android/RegistrationActivity.java
@@ -0,0 +1,10 @@
+package com.zappstech.socalintegration.android;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Ram on 10/20/2017.
+ */
+
+public class RegistrationActivity extends AppCompatActivity {
+}
